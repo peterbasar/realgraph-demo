f7f20778eb67dcb750e5176285390da1b04ee1a8
peterbasar <30775179+peterbasar@users.noreply.github.com>
2024-07-31 20:32:37 +0200

    realgraph package presenting basic import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,50 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import RealGraph from "@/components/RealGraph";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <section className={styles['section-intro']}>
+        <header className={styles['header']}>
+          <h1 className={styles['package-name']}>
+            realgraph
+          </h1>
+
+          <h4 className={styles['package-description']}>
+            real time graph data visualization library for React
+          </h4>
+        </header>
+
+        <div className={styles['section-intro-example']}>
+          <div className={styles['package-install-wrapper']}>
+            <code className={styles['package-install']}>
+              npm install realgraph
+            </code>
+          </div>
+
+          <div className={styles['graph-example-container']}>
+            <div className={styles['graph-example-code-wrapper']}>
+              <code className={styles['graph-example-code']}>
+                {`import Graph from 'realgraph'`}<br/>
+                <br/>
+                {'function Example() {'}<br/>
+                {'    return <Graph/>'}<br/>
+                {'}'}
+              </code>
+            </div>
+
+            <div className={styles['graph-example-graph']}>
+              <RealGraph />
+            </div>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <code className={styles['github-link']}>
+          <Link href="https://github.com/peterbasar/realgraph">{'<code />'}</Link>
+        </code>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
   );
 }
