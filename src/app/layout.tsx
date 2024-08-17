23fafa0580df55231271a41e6556ea032062b885
peterbasar <30775179+peterbasar@users.noreply.github.com>
2024-08-17 13:00:42 +0200

    Vercel analytics added to the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/react'
 import { Inter } from 'next/font/google'
 import type { Metadata } from 'next'
